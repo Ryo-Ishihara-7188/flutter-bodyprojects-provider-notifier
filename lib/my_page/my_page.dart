087dfa1506910e8c83f1242d20119d76d8ecdca7
ishihara <r.funk.7188@gmail.com>
2020-10-19 11:02:44 +0900

    カードを追加

--- lib/my_page/my_page.dart
+++ lib/my_page/my_page.dart
@@ -10,7 +10,35 @@ class MyPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('体重管理APP'),
       ),
-      body: Text('頑張るか、超頑張るか  '),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 26),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(8)),
+            ),
+            Text("今日の体重を追加しましょう"),
+            IconButton(
+                icon: Icon(
+                  Icons.add_circle_outline,
+                  color: Colors.amberAccent,
+                ),
+                onPressed: () {
+                  print('ON PUSHED');
+                })
+          ],
+        ),
+      ),
     );
   }
 }
