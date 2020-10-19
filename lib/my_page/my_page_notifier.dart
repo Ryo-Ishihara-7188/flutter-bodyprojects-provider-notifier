61fbb77c8d208bf8d31171b24df8311005e1046c
ishihara <r.funk.7188@gmail.com>
2020-10-20 00:51:44 +0900

    登録メソッド追加

--- lib/my_page/my_page_notifier.dart
+++ lib/my_page/my_page_notifier.dart
@@ -12,6 +12,7 @@ abstract class MyPageState with _$MyPageState {
     @Default(0) int count,
     String weight,
     String comment,
+    @Default([]) List<Map<String, String>> record,
   }) = _MyPageState;
 }
 
@@ -39,6 +40,22 @@ class MyPageNotifier extends StateNotifier<MyPageState> with LocatorMixin {
   void saveComment(String value) {
     state = state.copyWith(comment: value);
     print(state.comment);
+  }
+
+  void register() {
+    final formRecord = {
+      'weight': state.weight,
+      'comment': state.comment,
+      'day': DateTime.now().toString()
+    };
+    print(formRecord);
+
+    final newRecord = List<Map<String, String>>.from(state.record);
+    newRecord.add(formRecord);
+    state = state.copyWith(record: newRecord);
+    print(state.record);
+
+    Navigator.pop(context);
   }
 
   void pushButton() {
